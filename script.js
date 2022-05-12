f0edc16fafe1e9d2ad5311eabbb836ed2f520f70
doctype-Melvin <doctype.melvin@gmail.com>
2022-05-12 09:03:17 +0200

    adds media queries and starting point for drawing functions

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const drawBoard = (() => {
+const sketchNSketch = (() => {
+    //Menu panel with different drawing modes
+    const menu = document.querySelector('.menu');
+    menu.textContent = 'Menu';
+        const reset = document.createElement('button');
+        reset.textContent = 'Reset';
+        menu.append(reset);
+            const rainbow = document.createElement('button');
+            rainbow.textContent = 'Rainbow';
+            menu.append(rainbow);
+                const greyScale = document.createElement('button');
+                greyScale.textContent = 'Greyscale';
+                menu.append(greyScale);
+                    const colorPicker = document.createElement('input');
+                    colorPicker.setAttribute('type', 'color');
+                    menu.append(colorPicker);
+                        const slider = document.createElement('input');
+                        slider.setAttribute('type', 'range');
+                        menu.append(slider);
+    
+    //Drawing board grid
     const board = document.querySelector('.board');
     board.style.gridTemplateRows = 'repeat(16, 1fr)'
     board.style.gridTemplateColumns = 'repeat(16, 1fr)';
@@ -6,5 +26,7 @@ const drawBoard = (() => {
         const cell = document.createElement('div');
         cell.classList.add('cell');
             board.append(cell)
-    }
+    };
+    //Event listeners
+    const grid = document.querySelectorAll('.cell');
 })()
